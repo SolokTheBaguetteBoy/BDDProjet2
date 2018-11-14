c0ebdc73a049e3467a4b5bc7862da731ae3400c4
georgemathieu <33687645+georgemathieu@users.noreply.github.com>
2018-11-14 13:53:37 +0100

    Update src/baseDeDonnée/Record.java
    record

---
+++ src/baseDeDonnée/Record.java
@@ -0,0 +1,19 @@
+package baseDeDonnée;
+
+import java.util.ArrayList;
+
+public class Record {
+	
+	private ArrayList<String> values;
+
+	public ArrayList<String> getValues() {
+		return values;
+	}
+
+	public void setValues(ArrayList<String> values) {
+		this.values = values;
+	}
+	
+	
+
+}
