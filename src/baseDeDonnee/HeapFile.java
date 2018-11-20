552f949bf046bc998aace40a9e1bdc7b80919573
ii02735 <muhammad.yadallee@etu.parisdescartes.fr>
2018-11-20 23:08:04 +0100

    Méthode getFreePageId presque terminée...

--- src/baseDeDonnee/HeapFile.java
+++ src/baseDeDonnee/HeapFile.java
@@ -1,13 +1,7 @@
 package baseDeDonnee;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 
-import baseDeDonnee.DiskManager;
-import baseDeDonnee.RelDef;
 import bufferManager.BufferManager;
 import headerPageInfo.CoupleEntiers;
 import headerPageInfo.HeaderPageInfo;
@@ -17,7 +11,6 @@ public class HeapFile {
 	private RelDef listeChainee;
 	private DiskManager dm;
 	private BufferManager bm;
-
 	public HeapFile() {
 		// TODO Auto-generated constructor stub
 		this.dm = DiskManager.getInstance();
@@ -31,12 +24,12 @@ public class HeapFile {
 		dm.createFile(iFileIdx);
 		dm.addPage(iFileIdx, pid);
 		header.writeToBuffer(bm.get(pid));
-		this.bm.free(pid, true);
-		
+		this.bm.free(pid, true);	
 	}
 	
-	public void getFreePageId(PageId oPageId)
+	public void getFreePageId(PageId oPageId) throws IOException
 	{
+		boolean libre = false;
 		byte buffer[] = this.bm.get(oPageId);
 		HeaderPageInfo header = new HeaderPageInfo();
 		header.readFromBuffer(buffer);
@@ -44,9 +37,38 @@ public class HeapFile {
 		{
 			if(c.getFreeSlots() > 0)//vérifier qu'il reste des pages libres
 			{
+				oPageId.setPageIdx(c.getPageIdx());//On remplit la page de libre
+				this.bm.free(oPageId, false); //Libération sans modification
+				libre = true;
+				break;
 				
 			}
 		}
+		if(!libre)
+		{
+			this.dm.addPage(oPageId.getFileIdx(), oPageId);
+			this.bm.flushAll();//Actualisation complète ?
+			header.writeToBuffer(buffer);
+			this.bm.free(oPageId, true);
+			
+			//byte bufferNouvellePage[] = this.bm.get(oPageId);
+			//Utilisation du bytemap... comment faire ?
+		}
+	}
+	
+	public void updateHeaderWithTakenSlot(PageId iPageId)
+	{
+		byte buffer[] = this.bm.get(iPageId);
+		HeaderPageInfo header = new HeaderPageInfo();
+		header.writeToBuffer(buffer);
+		
+		/*
+		 * Impossible de changer le nombre de slots disponibles (changer CoupleEntiers correspondant à iPageId dans HeaderPageInfo)
+		 * --> setter pour le couple de HeaderPageInfo afin de changer le nombre de slots dispos ?
+		 * 
+		 * this.bm.flushAll(); //Actualisation
+		 * this.bm.free
+		 */
 	}
 	
 	public RelDef getListe() {
