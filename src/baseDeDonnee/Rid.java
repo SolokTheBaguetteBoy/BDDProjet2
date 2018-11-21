2824fd035dbffc11dc25ef4f3c6fdffd60745533
Solok the Baguette Boy <solok-sardine@hotmail.com>
2018-11-21 13:28:39 +0100

    TP4 Partie B
    Classe Rid

---
+++ src/baseDeDonnee/Rid.java
@@ -0,0 +1,23 @@
+package baseDeDonnee;
+
+public class Rid {
+	
+	private PageId pid;
+	private int sltIdx;
+	
+	public PageId getPid() {
+		return pid;
+	}
+	public void setPid(PageId pid) {
+		this.pid = pid;
+	}
+	public int getSltIdx() {
+		return sltIdx;
+	}
+	public void setSltIdx(int sltIdx) {
+		this.sltIdx = sltIdx;
+	}
+	
+	
+	
+}
